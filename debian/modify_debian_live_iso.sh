a1397a83190ef9f9f8371529c72844f3e89548b8
Bin Yong <binaryy@163.com>
2024-10-16 22:14:35 +0000

    Update modify_debian_live_iso.sh

--- debian/modify_debian_live_iso.sh
+++ debian/modify_debian_live_iso.sh
@@ -13,9 +13,16 @@ mkdir /tmp/cursed_dvd
 cd /tmp/cursed_dvd
 sudo umount *
 sudo rm -r *
+sudo rm new_root/dev
+
 
 mkdir iso_mount root_mount root_overlay_upper root_overlay_work new_root
 
 sudo mount -o loop "$ISO_FILE" iso_mount
 sudo mount iso_mount/live/filesystem.squashfs root_mount -t squashfs -o loop
 sudo mount -t overlay -o lowerdir=root_mount,upperdir=root_overlay_upper,workdir=root_overlay_work overlay new_root
+sudo mv new_root/dev new_root/fs_dev
+sudo ln -s /dev new_root/
+
+
+
