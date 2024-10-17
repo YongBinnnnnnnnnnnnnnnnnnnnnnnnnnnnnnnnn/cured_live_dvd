94e2baa147e75d1dde67e511102503aba4876496
Bin Yong <binaryy@163.com>
2024-10-17 23:33:00 +0000

    Update modify_debian_live_iso.sh

--- debian/modify_debian_live_iso.sh
+++ debian/modify_debian_live_iso.sh
@@ -60,12 +60,14 @@ sed -e "s|findiso=.*|verify nodhcp efi=noruntime module_blacklist=i2c_piix4,i2c_
 
 mkdir -p new_iso/live/
 sudo mksquashfs new_root new_iso/live/filesystem.squashfs -comp zstd -b 512K -Xcompression-level 22
-cat iso_mount/md5sum.txt | grep -v -e " ./install" -e " ./pool" -e " ./dists" > new_iso/md5sum.txt
-cat iso_mount/sha256sum.txt | grep -v -e " ./install" -e " ./pool" -e " ./dists" > new_iso/sha256sum.txt
 
-#TODO new hashes
-find new_iso/boot -type f -exec bash -c "iso_path=\$(echo {}|sed -e 's|new_iso|\\.|');hash=\$(sha256sum {}|cut -d ' ' -f 1);sed -i iso_mount/sha256sum.txt -e 's|.*\$iso_path\$|\$hash \$iso_path|';"\;
-find new_iso/boot -type f -exec bash -c "iso_path=\$(echo {}|sed -e 's|new_iso|\\.|');hash=\$(md5sum {}|cut -d ' ' -f 1);sed -i iso_mount/md5sum.txt -e 's|.*\$iso_path\$|\$hash \$iso_path|';"\;
+cat iso_mount/md5sum.txt | grep -v -e " ./install" -e " ./pool" -e " ./dists" > md5sum.txt
+cat iso_mount/sha256sum.txt | grep -v -e " ./install" -e " ./pool" -e " ./dists" > sha256sum.txt
+#new hashes
+find new_iso/boot -type f -exec bash -c "iso_path=\$(echo {}|sed -e 's|new_iso|\\.|');hash=\$(sha256sum {}|cut -d ' ' -f 1);sed -i sha256sum.txt -e 's|.*\$iso_path\$|\$hash \$iso_path|';"\;
+find new_iso/boot -type f -exec bash -c "iso_path=\$(echo {}|sed -e 's|new_iso|\\.|');hash=\$(md5sum {}|cut -d ' ' -f 1);sed -i md5sum.txt -e 's|.*\$iso_path\$|\$hash \$iso_path|';"\;
+mv md5sum.txt new_iso/
+mv md5sum.txt new_iso/
 
 #TODO ublock
 
