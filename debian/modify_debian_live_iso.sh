7dcb572cb13883d9ca7ff86002edab4550731c2b
Bin Yong <binaryy@163.com>
2024-10-17 22:39:55 +0000

    Update modify_debian_live_iso.sh

--- debian/modify_debian_live_iso.sh
+++ debian/modify_debian_live_iso.sh
@@ -57,7 +57,8 @@ sudo umount new_root/var/cache/apt
 mkdir -p new_iso/boot/grub/
 sed -e "s|800x600|1920x1080|g" iso_mount/boot/grub/config.cfg > new_iso/boot/grub/config.cfg
 sed -e "s|findiso=.*|verify nodhcp efi=noruntime module_blacklist=i2c_piix4,i2c_smbios,msr,parport,qrtr|g" iso_mount/boot/grub/grub.cfg > new_iso/boot/grub/grub.cfg
-cat iso_mount/md5sum.txt | grep -v -e " ./install" -e " ./pool" -e " ./dists"
+cat iso_mount/md5sum.txt | grep -v -e " ./install" -e " ./pool" -e " ./dists" > new_iso/md5sum.txt
+cat iso_mount/sha256sum.txt | grep -v -e " ./install" -e " ./pool" -e " ./dists" > new_iso/sha256sum.txt
 
 mkdir -p new_iso/live/
 sudo mksquashfs new_root new_iso/live/filesystem.squashfs -comp zstd -b 1024K
