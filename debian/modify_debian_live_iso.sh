83defea59a2afd63d96728f6234ac6d42effce18
Bin Yong <binaryy@163.com>
2024-10-18 17:14:44 +0000

    Add files via upload

--- debian/modify_debian_live_iso.sh
+++ debian/modify_debian_live_iso.sh
@@ -29,7 +29,7 @@ sudo mount -t overlay -o lowerdir=root_mount,upperdir=root_overlay_upper,workdir
 sudo mount -o bind /dev new_root/dev
 sudo mount -o bind /var/cache/apt new_root/var/cache/apt
 
-sudo chroot new_root apt install -y chromium bash-completion qemu-system-x86 git xorriso wodim
+sudo chroot new_root apt install -y chromium bash-completion qemu-system-x86 git xorriso wodim ibus-pinyin
 
 if sha256sum $CURSED/debian/cnijfilter2-6.71-1-deb.1a0080b3ee4b2d20a764f5ba5ff4bfd49be6f487b7ebbd9e5996290c29b7d9c2.tar.gz | cut -d " " -f 1| grep 1a0080b3ee4b2d20a764f5ba5ff4bfd49be6f487b7ebbd9e5996290c29b7d9c2; then
   tar -xvf $CURSED/debian/cnijfilter2-6.71-1-deb.1a0080b3ee4b2d20a764f5ba5ff4bfd49be6f487b7ebbd9e5996290c29b7d9c2.tar.gz cnijfilter2-6.71-1-deb/packages/cnijfilter2_6.71-1_amd64.deb --one-top-level=new_root --strip-components 2
@@ -38,13 +38,24 @@ if sha256sum $CURSED/debian/cnijfilter2-6.71-1-deb.1a0080b3ee4b2d20a764f5ba5ff4b
 fi
 
 sudo chroot new_root systemctl mask avahi-daemon fwupd cups-browsed 
-sudo chroot new_root apt autoremove --purge -y exim4-base bluez-firmware xiterm+thai gnome-games fcitx* fonts-thai-tlwg
+sudo chroot new_root apt autoremove --purge -y exim4-base bluez-firmware xiterm+thai gnome-games fcitx* fonts-thai-tlwg gnome-online-accounts gnome-initial-setup
+sudo chroot new_root dpkg -r gnome-user-docs yelp debian-reference-common debian-reference-es debian-reference-it fortunes-debian-hints
+sudo chroot new_root bash -c 'apt list --installed|grep "^task-"|grep -v -e english -e laptop|cut -d / -f 1| xargs dpkg -r'
+sudo chroot new_root bash -c 'apt list --installed|grep firefox-esr-l10n|cut -d / -f 1|xargs dpkg -r'
+sudo chroot new_root bash -c 'apt list --installed|grep spell-[a-z]|grep -v -e -en|cut -d / -f 1|xargs dpkg -r'
+sudo chroot new_root bash -c 'apt list --installed|grep -e fonts-lohit -e fonts-be -e fonts-t fonts-smc|cut -d / -f 1|xargs dpkg -r'
+sudo chroot new_root bash -c 'apt list --installed|grep manpages-|cut -d / -f 1|xargs dpkg -r'
+
+
+
 sudo cp $CURSED/hood/scripts/hosts new_root/etc/
 sudo cp $CURSED/hood/scripts/NetworkManager.conf new_root/etc/NetworkManager/NetworkManager.conf
 sudo cp $CURSED/hood/scripts/ca-certificates.conf new_root/etc/
 sudo cp $CURSED/hood/scripts/sysctl.conf new_root/etc/
 sudo mkdir -p new_root/etc/pki/
 sudo cp -r $CURSED/hood/scripts/nssdb new_root/etc/pki/
+sudo chroot new_root gsettings set org.gnome.desktop.thumbnailers disable-all true
+
 
 #sudo chmod -x new_root/usr/sbin/dhclient
 
@@ -56,7 +67,8 @@ sudo umount new_root/var/cache/apt
 
 mkdir -p new_iso/boot/grub/
 sed -e "s|800x600|1920x1080|g" iso_mount/boot/grub/config.cfg > new_iso/boot/grub/config.cfg
-sed -e "s|findiso=.*|verify-checksums nodhcp efi=noruntime module_blacklist=i2c_piix4,i2c_smbios,msr,parport,qrtr|g" iso_mount/boot/grub/grub.cfg > new_iso/boot/grub/grub.cfg
+sed -e "s|findiso=.*|nodhcp efi=noruntime module_blacklist=i2c_piix4,i2c_smbios,msr,parport,qrtr|g" iso_mount/boot/grub/grub.cfg > new_iso/boot/grub/grub.cfg
+#verify-checksums 
 
 mkdir -p new_iso/live/
 sudo mksquashfs new_root new_iso/live/filesystem.squashfs -comp zstd -b 512K -Xcompression-level 22
@@ -74,4 +86,4 @@ mv sha256sum.txt new_iso/
 xorriso -boot_image any keep -indev "$ISO_FILE" -outdev cursed.iso  -map new_iso / -rm_r /install -rm_r /pool -rm_r /dists -rm_r /pool-udeb
 
 cd -
-mv /tmp/cursed_dvd/cursed.iso cursed-`sha256sum | cut -d " " -f 1`-$(date "+%Y%m%d%H%M%S").iso
+mv /tmp/cursed_dvd/cursed.iso cursed-`sha256sum /tmp/cursed_dvd/cursed.iso | cut -d " " -f 1`-$(date "+%Y%m%d%H%M%S").iso
