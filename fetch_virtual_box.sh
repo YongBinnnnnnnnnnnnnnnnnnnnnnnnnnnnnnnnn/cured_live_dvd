36acefc6e8a10bd1da29c2c8d8f81601c4d2aac6
Debian Live user <user@localhost.localdomain>
2024-10-18 20:34:08 +0000

    add: fetch virtualbox

---
+++ fetch_virtual_box.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+mkdir download
+cd download
+curl -L -O -C - `curl -s https://www.virtualbox.org/wiki/Linux_Downloads|grep "Debian.*\.deb" -m 1| sed -r -e 's|.*href="([^"]*)".*|\1|'`
+curl -L -O -C - `curl -s https://www.virtualbox.org/wiki/Downloads|grep "\.vbox-extpack" -m 1| sed -r -e 's|.*href="([^"]*)".*|\1|'`
+curl -L -o virtualbox_sha256sums -C - https://www.virtualbox.org/`curl -s https://www.virtualbox.org/wiki/Downloads|grep "/SHA256SUMS" -m 1| sed -r -e 's|.*href="([^"]*)".*|\1|'`
+
