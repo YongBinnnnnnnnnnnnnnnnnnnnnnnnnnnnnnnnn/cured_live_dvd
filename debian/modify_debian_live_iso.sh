3446568e41b297b2d6e5687fff28ceed30474c46
Bin Yong <binaryy@163.com>
2024-10-19 01:00:44 +0000

    Update modify_debian_live_iso.sh

--- debian/modify_debian_live_iso.sh
+++ debian/modify_debian_live_iso.sh
@@ -85,7 +85,7 @@ sudo umount new_root/var/cache/apt
 
 mkdir -p new_iso/boot/grub/
 sed -e "s|800x600|1920x1080|g" iso_mount/boot/grub/config.cfg > new_iso/boot/grub/config.cfg
-sed -e "s|findiso=.*|nodhcp efi=noruntime module_blacklist=i2c_piix4,i2c_smbios,msr,parport,qrtr,intel_rapl_common|g,serio_raw" iso_mount/boot/grub/grub.cfg > new_iso/boot/grub/grub.cfg
+sed -e "s|findiso=.*|nodhcp efi=noruntime module_blacklist=i2c_piix4,i2c_smbios,msr,parport,qrtr,intel_rapl_common,serio_raw initcall_blacklist=serial_base_port_init|g" iso_mount/boot/grub/grub.cfg > new_iso/boot/grub/grub.cfg
 #verify-checksums 
 
 mkdir -p new_iso/live/
