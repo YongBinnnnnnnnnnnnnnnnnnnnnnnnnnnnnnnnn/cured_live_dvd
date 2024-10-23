e5f3dc11f88e355e28a7dbb44e9cb60845709b44
Bin Yong <binaryy@163.com>
2024-10-23 22:06:52 +0000

    pinephonepro:disable serial tty

--- mobian/write_pinephone.sh
+++
@@ -1,1 +0,0 @@
-gzip -d -c mobian-pinephonepro-phosh-12.0.img.gz | sed -e "s|ttyS2|wyee0|g" -e "s|load-module module-suspend-on-idle|#oad-module module-suspend-on-idle|g"|sudo dd of=/dev/sdb bs=4M status=progress
---
+++ mobian/write_pinephonepro.sh
@@ -0,0 +1,1 @@
+gzip -d -c mobian-pinephonepro-phosh-12.0.img.gz | sed -e "s|ttyS2|wyee0|g" -e "s|load-module module-suspend-on-idle|#oad-module module-suspend-on-idle|g" -e s"s|ExecStart=-/sbin/agetty -o '-p -- \\u' --keep-baud 115200,57600,38400,9600 - \$TERM|ExecStart=-/bin/sleep infinity                                                    |g"|sudo dd of=/dev/sdb bs=4M status=progress
