a74b96c5e29eabe10a284e3f8a24def4511795dd
Bin Yong <binaryy@163.com>
2024-10-23 23:35:11 +0000

    Update write_pinephonepro_twoboot.sh

--- mobian/write_pinephonepro_twoboot.sh
+++ mobian/write_pinephonepro_twoboot.sh
@@ -1,1 +1,3 @@
+echo "seems useless"
+
 cat spi.installer.img | sed -e "s|console=ttyS2,115200n8 earlycon=uart8250,mmio32,0xff1a0000|                                                          |" |sudo dd of=/dev/sdb bs=4M status=progress
