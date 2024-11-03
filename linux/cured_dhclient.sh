adfa27df3a230b4e7d4b5e4587be2d93b595d8c7
Bin Yong <binaryy@163.com>
2024-11-03 23:15:13 +0000

    cured_dhclient: set dns

--- linux/cured_dhclient.sh
+++ linux/cured_dhclient.sh
@@ -2,6 +2,7 @@
 # Bin Yong all rights reserved.
 
 if realpath /proc/$PPID/exe | grep /usr/sbin/NetworkManager; then
+  echo nameserver 1.1.1.1> /etc/resolv.conf
   sleep infinity
   # todo detect addr change
   exit 0
